38edc4d5e4fe5ebc99ab709e9cea2bffe4372de7
Martin133458 <perdunstaryj76@gmail.com>
2024-11-22 13:44:41 +0500

    Add files via upload

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Hero from '../../components/Hero/Hero'
+import Content from '../../components/Contents/Content'
+
+
+const Home = () => {
+  return (
+   <>
+   <Header/>
+   <Hero/>
+   <Content/>
+   </>
+  )
+}
+
+export default Home
